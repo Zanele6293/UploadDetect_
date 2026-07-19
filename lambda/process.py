9cd9311b240c74ecd198acb07d25cdbe39f4622e
zanele <zanelefd@outlook.com>
2026-07-19 02:07:24 +0200

    finished the project

--- lambda/process.py
+++ lambda/process.py
@@ -2,7 +2,7 @@ import json
 
 def handler(event,context):
     for records in event['Records']:
-        bucket_name= records['s3'['bucket']['name']]
+        bucket_name= records['s3']['bucket']['name']
         file_name = records['s3']['object']['key']
         print(f"File detected! Bucket: {bucket_name}, File: {file_name}")
     return{
--- my_cloud_project/my_cloud_project_stack.py
+++ my_cloud_project/my_cloud_project_stack.py
@@ -1,6 +1,9 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_s3_notifications as s3n,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -11,9 +14,23 @@ class MyCloudProjectStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        my_bucket = s3.Bucket(self,"UploadBucket")
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyCloudProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        #This is the Robot(The lambda function)
+        my_function = _lambda.Function(
+            self,"ProcessFunction",
+            runtime = _lambda.Runtime.PYTHON_3_9,
+            handler = "process.handler",
+
+            #This line of code looks at your folder
+            code = _lambda.Code.from_asset("lambda")
+        )
+
+        #telling s3 to triger Lambda when a file is saved
+        my_bucket.add_event_notification(
+            s3.EventType.OBJECT_CREATED,
+            s3n.LambdaDestination(my_function)
+        )
+
+
+
